8e32126d9aa24022a9045f09a6e50a918374412f
Radostin Marinov <r.marinov045@gmail.com>
2022-04-20 21:23:44 +0300

    Initial setup

---
+++ front/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    ".src/**/*.{js, jsx, ts, tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#1c0c5b",
+        "secondary": "3d2c8d",
+        "tertiary": "916bbf",
+        "light": "c996cc"
+      }
+    },
+  },
+  plugins: [],
+}
