e8c1b3ff2dd56648efd88e6b83f50618dad3acf8
Radostin Marinov <r.marinov045@gmail.com>
2022-04-21 01:34:30 +0300

    Better styling

--- front/src/components/AboutMe.tsx
+++ front/src/components/AboutMe.tsx
@@ -18,16 +18,18 @@ function AboutMe() {
             <Navbar />
             <CSSTransition in={test} timeout={200} classNames='slide' unmountOnExit={true}>
                 <section id='about' className='w-full h-screen bg-primary flex'>
-                    <div className='h-full w-full flex justify-center items-center'>
-                        <div className='font-bold text-white p-2 flex flex-col gap-5 pl-20 w-2/3 items-start justify-center'>
-                            <h1 className='text-4xl text-tertiary'>About me</h1>
-                            <p className='text-xs w-2/3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum sapiente delectus cumque doloribus consequatur tenetur molestiae harum explicabo qui accusantium rem voluptate quo repellendus, ducimus, mollitia, distinctio nesciunt. Vero, eum?
-                                Tenetur nostrum sequi, quia dolores aliquam unde minima qui sed illum excepturi et nulla architecto? Expedita dolorem illo ut, quam ex voluptates officiis, possimus alias molestias rerum numquam voluptatibus! Dignissimos.
-                                Inventore laborum nam accusantium magni eaque quam alias consequatur ipsam qui earum aliquid dolor sapiente, corporis, corrupti non maxime sunt officiis autem? Fugit velit est aliquam, ullam commodi quaerat vitae.
-                                Eos dolore ipsam commodi saepe? Molestias cum dolorum amet quis corporis, quae quaerat! Commodi dignissimos officiis beatae placeat veritatis consectetur. Veniam magnam quasi odit iste modi alias non totam impedit.
-                                Mollitia nesciunt neque vitae non voluptatibus, velit, aliquid adipisci molestias magni, minima at aspernatur distinctio blanditiis dolorum pariatur cumque tempora doloribus sequi doloremque. Assumenda omnis expedita, adipisci illo deserunt eligendi?</p>
+                    <div className='h-full w-4/5 mx-auto md:mx-0 md:w-full flex justify-center items-center md:pr-0'>
+                        <div className='font-bold text-white p-2 flex flex-col gap-5 md:pl-20 w-2/3 items-start justify-center'>
+                            <h1 className='md:text-4xl text-2xl text-tertiary'>About me</h1>
+                                <ul className='flex list-disc md:hidden text-xs flex-col gap-5'>
+                                    <li>Located in: Sofia, Bulgaria</li>
+                                    <li>22 years old</li>
+                                    <li>Passion to grow</li>
+                                    <li>Interested in the entire front end spectrum</li>
+                                    <li>Highly motivated and organized</li>
+                                </ul>
                         </div>
-                        <div className='w-1/4 rounded-full'>
+                        <div className='md:w-1/4 w-1/2 rounded-full'>
                             <img src={profileImg} alt="profile" className='rounded-full' />
                         </div>
                         <ArrorLink path='/stack' />
--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -19,13 +19,13 @@ function Home() {
                 <h1 className='hover:text-tertiary transition-all ease-in-out 150'>Hi,</h1>
                 <h1 className='hover:text-tertiary transition-all ease-in-out 150'>I'm Rado,</h1>
                 <h1 className='text-base hover:text-tertiary transition-all ease-in-out 150'>Front End Developer</h1>
-                <Link to='' className='mt-5 p-2 px-3 bg-lighter rounded-sm text-center text-sm hover:bg-light transition-all 150'>Contact me</Link>
+                <Link to='/contact' className='mt-5 p-2 px-3 bg-lighter rounded-sm text-center text-sm hover:bg-light transition-all 150'>Contact me</Link>
             </div>
             <div className='h-1/2 w-1/5 flex flex-col-reverse justify-end items-end'>
                 <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                     <path fill="#6BD0FF" d="M59.6,-33.4C73.3,-10.6,77.7,18.4,66.3,38.8C54.8,59.1,27.4,70.8,2.2,69.5C-23,68.3,-46,54,-60,32.2C-74.1,10.3,-79.3,-19.2,-67.8,-40.7C-56.3,-62.2,-28.1,-75.7,-2.6,-74.2C22.9,-72.7,45.9,-56.2,59.6,-33.4Z" transform="translate(100 100)" />
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-32 w-32 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="md:h-32 md:w-32 h-24 w-24 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
                 </svg>
             </div>
